d9b6744d679f651023b23261227f9db69ad63a79
rahulkumar <68043021+RahulKumar0920@users.noreply.github.com>
2024-12-05 15:01:20 +0530

    added code to HardCode Questions * Display them

--- Question.java
+++ Question.java
@@ -64,4 +64,11 @@ public class Question{
         this.answer = answer;
     }
 
+
+    //toString
+    @Override
+    public String toString() {
+        return "Question [id=" + id + ", question=" + question + ", opt1=" + opt1 + ", opt2=" + opt2 + ", opt3=" + opt3
+                + ", opt4=" + opt4 + ", answer=" + answer + "]";
+    }
 }
--- QuestionService.java
+++ QuestionService.java
@@ -1,3 +1,16 @@
 public class QuestionService{
-    
+
+    Question[] questions = new Question[2];
+
+    public QuestionService(){
+        questions[0] = new Question(1, "size of int : ", "2","4","6","8","4");
+        questions[1] = new Question(2, "size of char : ", "2","4","6","8","2");
+    }
+
+    public void displayQuestions(){
+
+        for(Question q : questions){
+            System.out.println(q);
+        }
+    }
 }
